4a612aa1e13d30c9b5be1d3e4f65e3c9d0dddf81
Jeremy Mates <jmates@cpan.org>
2013-03-05 05:22:07 +0000

    Code, perldocs, tests to reasonable state.

--- lib/File/Cmp.pm
+++ lib/File/Cmp.pm
@@ -1,6 +1,6 @@
 # -*- Perl -*-
 #
-# compare two files character by character like cmp(1)
+# Compare two files character by character like cmp(1).
 
 package File::Cmp;
 
@@ -8,31 +8,163 @@ use 5.008000;
 use strict;
 use warnings;
 
+use Carp qw/croak/;
+use Scalar::Util qw/reftype/;
+
 require Exporter;
-our @ISA = qw(Exporter);
-our @EXPORT_OK = qw/cmp/;
+our @ISA       = qw(Exporter);
+our @EXPORT_OK = qw/&fcmp/;
 
-our $VERSION = '0.01';
+our $VERSION = '1.00';
 
+# XXX 'skip' and 'limit' might be good parameters to add, to skip X
+# initial bytes, limit work to Y bytes of data to check, a param with a
+# scalar reference to be fed the byte of the first difference, etc.
+sub fcmp {
+  croak 'fcmp needs two files' if @_ < 2;
+  my @files = splice @_, 0, 2;
+  my $param = ( @_ == 1 and ref $_[0] eq 'HASH' ) ? $_[0] : {@_};
 
+  $param->{sizecheck} = 1 unless exists $param->{sizecheck};
+
+  if ( $param->{fscheck} ) {
+    my @statbuf;
+    for my $f (@files) {
+      # stat has the handy property of chasing symlinks for us
+      my @devino = ( stat $f )[ 0, 1 ] or croak "could not stat: $!";
+      push @statbuf, \@devino;
+    }
+    if (  $statbuf[0][0] == $statbuf[1][0]
+      and $statbuf[0][1] == $statbuf[1][1] ) {
+      return 1;    # assume files identical as both dev and inode match
+    }
+  }
+
+  # The files are probably not identical if they differ in size;
+  # however, offer means to turn this check off if -s for some reason is
+  # incorrect.
+  if ( $param->{sizecheck} ) {
+    return 0 if -s $files[0] != -s $files[1];
+  }
+
+  my @fhs;
+  for my $f (@files) {
+    if ( !defined reftype $f) {
+      open my $fh, '<', $f or croak "could not open $f: $!";
+      push @fhs, $fh;
+    } else {
+      # Assume is a GLOB or something can readline on, XXX might want to
+      # better check this
+      push @fhs, $f;
+    }
+    if ( exists $param->{binmode} ) {
+      binmode $fhs[-1], $param->{binmode} or croak "binmode failed: $!";
+    }
+  }
+
+  local $/ = $param->{RS} if exists $param->{RS};
+
+  while (1) {
+    my $eof1 = eof $fhs[0];
+    my $eof2 = eof $fhs[1];
+    # Done if both files are at EOF; otherwise assume they differ if one
+    # completes before the other (this second case would normally be
+    # optimized away by the -s test, above).
+    last if $eof1 and $eof2;
+    return 0 if $eof1 xor $eof2;
+
+    my $this = readline $fhs[0];
+    croak "error reading from first file: $!" if !defined $this;
+    my $that = readline $fhs[1];
+    croak "error reading from second file: $!" if !defined $that;
+
+    return 0 if $this ne $that;    # differ, we're outta here
+  }
+
+  return 1;                        # assume files identical if get this far
+}
 
 1;
 __END__
 
 =head1 NAME
 
-File::Cmp - compare two files character by character like cmp(1)
+File::Cmp - compare two files character by character
 
 =head1 SYNOPSIS
 
-  use File::Cmp qw/cmp/;
-  TODO
+  use File::Cmp qw/fcmp/;
+
+  print "identical" if fcmp("/tmp/foo", "/tmp/bar");
+
+  fcmp(
+    $fh1, $fh2,
+    binmode   => ':raw',  # a good default
+    fscheck   => 1,       # ... but beware network fs/portability
+    RS        => \"4096"  # handy for binary
+  );
 
 =head1 DESCRIPTION
 
-TODO
+This module offers a B<fcmp> function that checks whether the contents
+of two files are identical. A single subroutine, B<fcmp>, is offered for
+optional export. It expects at minimum two files or file handles, along
+with various optional parameters following those filenames. Any errors
+encountered will cause an exception to be thrown; consider C<eval> or
+L<Try::Tiny> to catch these. Otherwise, the return value will be true if
+the files are identical, false if not.
+
+Note that if passed a file handle, the code will read to the end of the
+handle, and will not rewind. This will require C<tell> and C<seek>
+function calls before and after B<fcmp> to return to the same position,
+if necessary. Likewise, if entire file contents are to be compared, file
+handles may need C<SEEK_SET> performed on them to move to the beginning
+prior to the B<fcmp> call. None of this is a concern if file names are
+passed instead of file handles.
+
+Available parameters include:
+
+=over 4
+
+=item I<binmode>
+
+If set, applied as the C<LAYER> specification of a C<binmode> call
+performed on the files or file handles. C<:raw> may very well likely be
+prudent for most cases, to avoid wasting time on linefeeds and the like.
+
+If the files need different C<binmode> settings, for example if
+comparing irrespective of the linefeeds involved, do not set this
+option, and instead pass in the file handles with C<binmode> already set
+as appropriate on those file handles.
+
+=item I<fscheck>
+
+If set and true, perform C<stat> tests on the input to check whether the
+device and inode numbers are identical. If so, this will avoid the need
+to check the file contents. This test may run afoul network filesystems
+or other edge cases possibly mentioned in L<perlport>.
+
+=item I<RS>
+
+Input Record Separator value for C<$/>, see the docs for such in
+L<perlvar>. Binary file comparisons will likely benefit from the use
+of a fixed record size, as who knows how long the "lines" could be in
+such files.
+
+=item I<sizecheck>
+
+If set and false, disables the default C<-s> file size test on the input
+files. This will force the file contents to be checked, even if the
+files are of differing size (and thus stand a good chance of not being
+identical). (Sparse files (untested) or unknown unknowns prompt the
+inclusion of this knob.)
+
+=back
 
 =head1 BUGS
+
+No attempt at portability is made; in particular, this module assumes
+Unix file system semantics for the C<fscheck> parameter.
 
 Newer versions of this module may be available from CPAN. If the bug is
 in the latest version, check:
@@ -41,7 +173,7 @@ L<http://github.com/thrig/File-Cmp>
 
 =head1 SEE ALSO
 
-L<cmp(1)>
+L<cmp(1)>, L<perlfunc>, L<perlport>, L<perlvar>
 
 =head1 AUTHOR
 
--- t/File-Cmp.t
+++ t/File-Cmp.t
@@ -3,5 +3,37 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
-BEGIN { use_ok('File::Cmp') }
+use Test::More;    # plan is down near bottom
+
+BEGIN { use_ok( 'File::Cmp', qw/fcmp/ ) }
+
+ok( fcmp( 'MANIFEST', 'MANIFEST' ), 'manifestly the same' );
+
+ok( !fcmp( 'MANIFEST', \*DATA ), 'differ' );
+
+# seek() to reset should not be necessary, as the 'fscheck' code should
+# stat this file, discover that both filehandle reference point to this
+# file, and avoid looking at the contents.
+#seek DATA, 0, 0;
+ok( fcmp( \*DATA, \*DATA, fscheck => 1 ), 'fscheck' );
+
+# Default line-based reads are actually tricker than just setting RS to
+# something - RS => \"4096" or whatever and binmode => ':raw' might
+# actually be better defaults to set...
+ok( !fcmp( 'this', 'that' ), 'extra line, sizecheck' );
+ok( !fcmp( 'this', 'that', sizecheck => 0, blah => 1 ),
+  'extra line, no sizecheck' );
+
+# XXX otherwise, debugging the internal action of fcmp is tricky; I
+# added warn statements to it to verify what I thought was happening,
+# but this isn't very robust if there is future development... perhaps
+# add a scalar reference param that would be populated with a reason for
+# the difference, or a verbose param?
+
+# XXX sparse files might be good to test, but would likely have to
+# generate such files, assuming the target system supports them, etc.
+
+plan tests => 6;
+
+__DATA__
+An, Android
